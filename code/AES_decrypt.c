4ac22d93c0a1e2b5e3e2b2a895c46251c0ca7863
froznsm <freezer_xx@gmx.de>
2020-12-21 13:46:36 +0100

    added multiply function, tested as working

--- code/AES_decrypt.c
+++ code/AES_decrypt.c
@@ -2,6 +2,8 @@
 #include <omp.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
+
 
 const uint8_t sbox[256] =
 {   0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x1, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
@@ -41,6 +43,11 @@ const uint8_t inv_sbox[256] = {
 };
 
 
+const uint8_t inverse_mult_matrix[16] = {
+    0x0e, 0x09, 0x0d, 0x0b, 0x0b, 0x0e, 0x09, 0x0d, 0x0d, 0x0b, 0x0e, 0x09, 0x09, 0x0d, 0x0b, 0x0e
+};
+
+
 void addRoundKey(uint8_t *block, const uint8_t *keys)
 {
     for(uint8_t i = 0; i < 16; i++)
@@ -53,21 +60,20 @@ void addRoundKey(uint8_t *block, const uint8_t *keys)
 void inverseShiftRows(uint8_t *block)
 {
 
-  uint8_t tmp[16];
-  memcpy(tmp, block, 16 * sizeof(uint8_t));
-
-  block[1] = tmp[13];
-  block[2] = tmp[10];
-  block[3] = tmp[7];
-  block[5] = tmp[1];
-  block[6] = tmp[14];
-  block[7] = tmp[11];
-  block[9] = tmp[5];
-  block[10] = tmp[2];
-  block[11] = tmp[15];
-  block[13] = tmp[9];
-  block[14] = tmp[6];
-  block[15] = tmp[3];
+    uint8_t tmp[16];
+    memcpy(tmp, block, 16 * sizeof(uint8_t));
+    block[1] = tmp[13];
+    block[2] = tmp[10];
+    block[3] = tmp[7];
+    block[5] = tmp[1];
+    block[6] = tmp[14];
+    block[7] = tmp[11];
+    block[9] = tmp[5];
+    block[10] = tmp[2];
+    block[11] = tmp[15];
+    block[13] = tmp[9];
+    block[14] = tmp[6];
+    block[15] = tmp[3];
 }
 
 
@@ -80,9 +86,51 @@ void inverseSubBytes(uint8_t *block)
 }
 
 
+
+//peassants algorithm as per the wikipedia article on finite field arithmatic
+uint8_t multiply(uint8_t a, uint8_t b)
+{
+    uint16_t product = 0;
+
+    for(uint8_t i = 0; i < 8; i++)
+    {
+        if((b & 1) != 0)
+            product ^= a;
+        b = b >> 1;
+        bool carry = ((a & 0x80) != 0);
+        a = a << 1;
+        if(carry)
+            a ^= 0x1B;
+    }
+
+    return product;
+}
+
+
 void inverseMixColumns(uint8_t *block)
 {
-    //mix em
+    uint8_t tmp[16];
+    memcpy(tmp, block, 16 * sizeof(uint8_t));
+
+    for(uint8_t i = 0; i < 16; i+=4) {
+        block[i] = multiply(tmp[i], inverse_mult_matrix[i]) ^
+                   multiply(tmp[i+1], inverse_mult_matrix[i+1]) ^
+                   multiply(tmp[i+2], inverse_mult_matrix[i+2]) ^
+                   multiply(tmp[i+3], inverse_mult_matrix[i+3]);
+        block[i+1] = multiply(tmp[i], inverse_mult_matrix[i]) ^
+                   multiply(tmp[i+1], inverse_mult_matrix[i+1]) ^
+                   multiply(tmp[i+2], inverse_mult_matrix[i+2]) ^
+                   multiply(tmp[i+3], inverse_mult_matrix[i+3]);
+        block[i+2] = multiply(tmp[i], inverse_mult_matrix[i]) ^
+                   multiply(tmp[i+1], inverse_mult_matrix[i+1]) ^
+                   multiply(tmp[i+2], inverse_mult_matrix[i+2]) ^
+                   multiply(tmp[i+3], inverse_mult_matrix[i+3]);
+        block[i+3] = multiply(tmp[i], inverse_mult_matrix[i]) ^
+                   multiply(tmp[i+1], inverse_mult_matrix[i+1]) ^
+                   multiply(tmp[i+2], inverse_mult_matrix[i+2]) ^
+                   multiply(tmp[i+3], inverse_mult_matrix[i+3]);
+
+    }
 }
 
 
