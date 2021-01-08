c39aa151bfa142446da2dd5fd52db7dbb8c8e492
froznsm <freezer_xx@gmx.de>
2021-01-08 11:30:33 +0100

    added basic cli for block decryption

--- code/wrapper_decrypt.py
+++ code/wrapper_decrypt.py
@@ -5,6 +5,8 @@ from subprocess import check_output
 from getpass import getpass
 import hashlib
 from key_expansion import expand_key
+import click
+import binascii
 
 compile_gcc = """gcc -O2 -w -shared -fpic -Wl,-soname,AES_decrypt
                 -o libaesdecrypt.so AES_decrypt.c -fopenmp""".split()
@@ -32,3 +34,22 @@ if not isfile("libaesdecrypt.so"):
                                     on your own.""")
 
 aeslib = ctypes.CDLL(join(getcwd(),"libaesdecrypt.so"))
+
+
+@click.command()
+@click.option("--text", help="ciphertext", prompt="Text to be decrypted")
+@click.option("--key", help="128 bit AES key", prompt="AES-128 Key")
+def decrypt(text, key):
+    cipherblock = bytearray.fromhex(text)
+    keys = expand_key(key)
+    byte_array = ctypes.c_ubyte * len(cipherblock)
+    byte_array_keys = ctypes.c_ubyte * len(keys)
+    aeslib.decryptBlock(
+        byte_array.from_buffer(cipherblock),
+        byte_array_keys.from_buffer(keys)
+    )
+    print(binascii.hexlify(cipherblock).decode("utf-8"))
+
+
+if __name__ == '__main__':
+    decrypt()
